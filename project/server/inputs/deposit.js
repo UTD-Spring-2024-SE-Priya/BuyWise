ebf16d3bb9cad304c7af008abf9c4ff5abd12606
rchilak1 <127245405+rchilak1@users.noreply.github.com>
2024-03-29 17:25:39 -0500

    deposit function/test cases

---
+++ project/server/inputs/deposit.js
@@ -0,0 +1,44 @@
+const deposit = async (username, depositAmount, group) => {
+    // Check if depositAmount is null or empty
+    if (!depositAmount || depositAmount.trim() === '') {
+        throw new Error("Value cannot be empty");
+    }
+    let data = null;
+
+    // Check if depositAmount is a valid number
+    if (!/^\d+$/.test(depositAmount.trim())) {
+        throw new Error("Value is not a number");
+    }
+    let response = null; // Declare a variable to hold the response
+    try {
+        // Await the fetch request and store the response
+        response = await fetch(`http://localhost:5050/${username}/${group}`);
+        if (!response.ok) {
+            throw new Error("Username group combo not found");
+        }
+        // Parse the response body as JSON
+        data = await response.json();
+    } catch (error) {
+        throw new Error("Failed to fetch user data");
+    }
+
+    try {
+        // Perform deposit by updating balance
+        let updateResponse = await fetch(`http://localhost:5050/deposit/${username}/${group}`, {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "balance": data.groups[0].totalBalance + parseFloat(depositAmount)
+            })
+        });
+        if (!updateResponse.ok) {
+            throw new Error("Deposit unsuccessful");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export default deposit;
---
+++ project/server/inputs/deposit.test.js
@@ -0,0 +1,23 @@
+import deposit from "./deposit";
+import { validateUserRegistration } from "./input_validation";
+
+describe('Deposit validation', () => {
+    test('Valid TestCase', async () => {
+        const addInitialUser = ['username5', 'Password1!', 'Password1!'];
+        await expect(validateUserRegistration(...addInitialUser)).resolves.not.toThrow();
+
+        const validInput = ['username5', '20', 'test'];
+        await expect(deposit(...validInput)).resolves.not.toThrow();
+    });
+
+    test('Invalid TestCase, Value entered not a number', async () => {
+        const validInput = ['username5', '99UJ', 'test'];
+        await expect(deposit(...validInput)).rejects.toThrow("Value is not a number");
+    });
+
+    test('Invalid TestCase, Value entered is empty', async () => {
+        const validInput = ['username5', '', 'test'];
+        await expect(deposit(...validInput)).rejects.toThrow("Value cannot be empty");
+    });
+
+});
