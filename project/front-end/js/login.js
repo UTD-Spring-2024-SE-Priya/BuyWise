c211cd8e704669af77462e4c43591bdd5b8299c7
VinayGaddamGG <vinayrgaddam@gmail.com>
2024-04-06 14:07:04 -0500

    moved frontend to project dir

--- project/front-end/js/login.js
+++ project/front-end/js/login.js
@@ -1,9 +1,11 @@
+import signIn from '../../server/inputs/signin_validation';
+
 const loginText = document.querySelector(".title-text .login");
 const loginForm = document.querySelector("form.login");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
-const loginSubmit = document.getElementsByClassName("field btn")
+const loginSubmit = document.getElementById("loginSubmit")
 signupBtn.onclick = (()=>{
  loginForm.style.marginLeft = "-50%";
  loginText.style.marginLeft = "-50%";
@@ -16,6 +18,19 @@ signupLink.onclick = (()=>{
  signupBtn.click();
  return false;
 });
-loginSubmit = (()=>{
-    window.location.href = "./main.html";
+loginSubmit.onclick = (() => {
+    loginCheck();
 });
+
+async function loginCheck() {
+    const username = document.getElementById("usernameField");
+    const password = document.getElementById("passwordField");
+    try {
+        await signIn(username, password);
+        console.log("success");
+    } catch (error) {
+        console.error("Error during sign-in:", error.message);
+    }
+
+}
+
