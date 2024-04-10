9cc434b7f76f2aff36906deea23b9c5d543c8dd3
VinayGaddamGG <vinayrgaddam@gmail.com>
2024-04-10 18:41:30 -0500

    testing & deposit/withdraw updated functions

--- project/server/inputs/deposit.js
+++ project/server/inputs/deposit.js
@@ -1,4 +1,4 @@
-const deposit = async (username, depositAmount, groupID) => {
+const deposit = async (depositAmount, groupID) => {
     // Check if depositAmount is null or empty
     if (!depositAmount || depositAmount.trim() === '') {
         throw new Error("Value cannot be empty");
@@ -13,39 +13,42 @@ const deposit = async (username, depositAmount, groupID) => {
     if (!/^\d+(\.\d+)?$/.test(depositAmount.trim())) {
         throw new Error("Value is not a number");
     }
-    let response = null; // Declare a variable to hold the response
-    try {
-        // Await the fetch request and store the response
-        response = await fetch(`http://localhost:5050/${username}/${groupID}`);
-        if (!response.ok) {
-            throw new Error("Username group combo not found");
-        }
-        // Parse the response body as JSON
-        data = await response.json();
-    } catch (error) {
-        throw new Error("Failed to fetch user data");
-    }
-
-    try {
-        const groupIndex = data.groups.findIndex(group => group._id === groupID);
-        if (groupIndex === -1) {
-            throw new Error("Group not found");
-        }
-        // Perform deposit by updating balance
-        let updateResponse = await fetch(`http://localhost:5050/update/${username}/${groupID}`, {
-            method: "PATCH",
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: JSON.stringify({
-                "balance": parseFloat(data.groups[groupIndex].balance) + parseFloat(depositAmount)
-            })
-        });
-        if (!updateResponse.ok){
-            throw new Error("Deposit unsuccessful");
+    try {  
+        const data = await fetch(`http://localhost:5050/find/user/group/${groupID}`);
+        const json = await data.json();
+        const userGroupIndex = json.groups.findIndex(group => group._id === groupID);
+        const initialBalance = parseFloat(json.groups[userGroupIndex].balance);
+        let eachUser = json.groups[userGroupIndex].users;
+        let arrayUsers = eachUser.split(",");
+        for (const user of arrayUsers){
+            try {
+                const curUser = await fetch(`http://localhost:5050/username/${user}`);
+                const jsonCurUser = await curUser.json();
+                const groupIndex = jsonCurUser.groups.findIndex(group => group._id === groupID);
+                if (groupIndex === -1) {
+                    throw new Error("Group found");
+                }
+                // Perform deposit by updating balance
+                let updateResponse = await fetch(`http://localhost:5050/update/${user}/${groupID}`, {
+                    method: "PATCH",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        "balance": initialBalance + parseFloat(depositAmount)
+                    })
+                });
+                if (!updateResponse.ok){
+                    throw new Error("Deposit unsuccessful");
+                }
+            } catch (error) {
+                console.log(error);
+                throw new Error(error);
+            }
         }
     } catch (error) {
         console.log(error);
+        throw new Error(error);
     }
 };
 
