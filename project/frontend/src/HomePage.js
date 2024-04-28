05e7093bccc88c9103a4f4a74ab2c2e7f64d4346
VinayGaddamGG <vinayrgaddam@gmail.com>
2024-04-27 21:15:10 -0500

    bug fix

--- project/frontend/src/HomePage.js
+++ project/frontend/src/HomePage.js
@@ -5,20 +5,20 @@ import './HomePage.css'
 const HomePage = () => {
   const [groups, setGroups] = useState([]); // State to store groups data
   const { username , groupID } = useParams(); // Get username from URL params
+  const [data , setData] = useState(null);
   const navigate = useNavigate();
   useEffect(() => {
-      // Fetch groups data asynchronously
       const fetchData = async () => {
           try {
               // Perform API call to fetch user data
               const response = await fetch(`http://localhost:5050/find/user/group/${groupID}`);
               const userData = await response.json();
-
               // Extract groups array from user data
               const { groups } = userData;
-              console.log(groups);
+              const index = await groups.findIndex(group => group._id === groupID);
               // Set the groups state with the fetched data
               setGroups(groups);
+              setData(index);
           } catch (error) {
               console.error('Error fetching data:', error);
           }
@@ -31,14 +31,14 @@ const HomePage = () => {
   return (
     <div className="dashboard-container">
         <header className="dashboard-header">
-            <h1>Account: {username}'s {groups.length > 0 && groups[0].name}</h1>
+            <h1>Account: {username}'s {groups.length > 0 && data !== undefined && groups[data].name}</h1>
         </header>
-        <h3>Users : {groups.length > 0 && groups[0].users.split(',').join(', ')}</h3>
+        <h3>Users : {groups.length > 0 && data !== undefined && groups[data].users.split(',').join(', ')}</h3>
         <section className="dashboard-details">
             <div className="account-details">
                 <h2>Account Details</h2>
-                <button className="button" onClick={() => navigate(`/deposit/${username}/${groupID}/${groups[0].name}`)}>Deposit</button>
-                <button className="button" onClick={() => navigate(`/Withdraw/${username}/${groupID}/${groups[0].name}`)}>Withdraw</button>
+                <button className="button" onClick={() => navigate(`/deposit/${username}/${groupID}/${groups[data].name}`)}>Deposit</button>
+                <button className="button" onClick={() => navigate(`/Withdraw/${username}/${groupID}/${groups[data].name}`)}>Withdraw</button>
                 <button className="button finance-management" onClick={() => navigate(`../GroupManagement/${username}`)}>
                     Back
                 </button>
@@ -46,12 +46,12 @@ const HomePage = () => {
             <div className="financial-summary">
                 <div className="financial-item balance">
                     <label>Account Balance</label>
-                    <div>${groups.length > 0 && groups[0].balance}</div>
+                    <div>${groups.length > 0 && data !== undefined && groups[data].balance}</div>
                 </div>
                 <div className="financial-item transaction">
                     <label>Transaction history</label>
                         
-                            {groups.length > 0 && groups[0].transactions.map((transaction, index) => (
+                            {groups.length > 0 && data !== undefined && groups[data].transactions.map((transaction, index) => (
                                 <menu key={index}>
                                     <div>{transaction}</div>
                                 </menu>
